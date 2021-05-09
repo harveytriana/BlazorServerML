10e2f39bd43269b24212eff268e6b9d734a40988
harveytriana <harveytriana@hotmail.com>
2021-05-08 23:01:47 -0500

    Bind

--- BlazorServerML/Utils.cs
+++ BlazorServerML/Utils.cs
@@ -9,9 +9,9 @@ namespace BlazorServerML
 {
     public static class Utils
     {
-        public static string SplitCamelCase(this string str)
+        public static string SplitCamelCase(string text)
         {
-            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+            return Regex.Replace(Regex.Replace(text, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
         }
 
         public static Dictionary<string, string> GetNamesOf<T>(T item) where T : class
