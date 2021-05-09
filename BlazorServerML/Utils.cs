38e852632ad234cfe0c945eb9e12e0a46f7fc66b
harveytriana <harveytriana@hotmail.com>
2021-05-08 22:24:48 -0500

    Form

---
+++ BlazorServerML/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BlazorServerML
+{
+    public static class Utils
+    {
+        public static string SplitCamelCase(this string str)
+        {
+            return Regex.Replace(Regex.Replace(str, @"(\P{Ll})(\P{Ll}\p{Ll})", "$1 $2"), @"(\p{Ll})(\P{Ll})", "$1 $2");
+        }
+
+        public static Dictionary<string, string> GetNamesOf<T>(T item) where T : class
+        {
+            var r = new Dictionary<string, string>();
+            try {
+                foreach (PropertyInfo property in item.GetType().GetProperties()) {
+                    var key = property.Name;
+                    var value = property.GetValue(item, null).ToString();
+                    if (property.PropertyType.Name == "Single") {
+                        value = float.Parse(value).ToString("0.00");
+                    }
+                    r.Add(key, value);
+                }
+            }
+            catch {
+            }
+            return r;
+        }
+    }
+
+}
